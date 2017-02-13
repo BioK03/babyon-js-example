b1f99a337cb0993478d2f42679851fd292fc3082
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2017-02-13 18:45:08 +0100

    Added overlay and beta camera control

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
+var camera = null;
+
+var fullScreen = false;
+
+
 window.addEventListener('DOMContentLoaded', function() {
     var canvas = document.getElementById('renderCanvas');
+    var overlay = document.getElementById('overlay');
 
     var engine = new BABYLON.Engine(canvas, true);
     var scene = null;
-    var camera = null;
     var light = null;
     var sphere = null;
     var ground = null;
     var movingVector = [0.0, 0.0];
     var arrObjects = [];
-
+    
     var createScene = function() {
         var scene = new BABYLON.Scene(engine);
 
-
-            // Lights
+        // Lights
         var light0 = new BABYLON.DirectionalLight("Omni", new BABYLON.Vector3(-2, -5, 2), scene);
         var light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(2, -5, -2), scene);
 
         // Need a free camera for collisions
-        var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, -8, -20), scene);
-        camera.attachControl(canvas, true);
+        camera = new BABYLON.FreeCamera("UniversalCamera", new BABYLON.Vector3(0, -8, -20), scene);
+        camera.attachControl(overlay, true);
         camera.speed = 3;
 
         arrObjects.push(BABYLON.Mesh.CreatePlane("ground", 200.0, scene));
@@ -31,7 +35,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
         arrObjects.push(BABYLON.Mesh.CreateBox("crate", 2, scene));
         arrObjects[arrObjects.length-1].material = new BABYLON.StandardMaterial("groundMat", scene);
-        arrObjects[arrObjects.length-1].material.diffuseColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+        arrObjects[arrObjects.length-1].material.diffuseColor = new BABYLON.Color3(0.2, 0.2, 0.2);
         arrObjects[arrObjects.length-1].position = new BABYLON.Vector3(5, -9, -8.2);
 
         arrObjects.push(BABYLON.Mesh.CreateBox("crate", 2, scene));
@@ -42,7 +46,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
         arrObjects.push(BABYLON.Mesh.CreateBox("crate", 2, scene));
         arrObjects[arrObjects.length-1].material = new BABYLON.StandardMaterial("groundMat", scene);
-        arrObjects[arrObjects.length-1].material.diffuseColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+        arrObjects[arrObjects.length-1].material.diffuseColor = new BABYLON.Color3(0.3, 0.3, 0.3);
         arrObjects[arrObjects.length-1].position = new BABYLON.Vector3(5, -9.415, -9.2);
         arrObjects[arrObjects.length-1].rotation = new BABYLON.Vector3(Math.PI / 4, 0, 0);
 
@@ -87,14 +91,65 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     var checkEvents = function(){
-        movingVector[0] *= 0.95;
-        movingVector[1] *= 0.95;
+        movingVector[0] *= 0.5;
+        movingVector[1] *= 0.5;
 
-        if(keys[90] && movingVector[0]<1) {movingVector[0] += 0.1}
-        if(keys[83] && movingVector[0]>-1) {movingVector[0] -= 0.1}
-        if(keys[68] && movingVector[1]<1) {movingVector[1] += 0.1}
-        if(keys[81] && movingVector[1]>-1) {movingVector[1] -= 0.1}
-        /*sphere.position.z += movingVector[0]*0.1;
-        sphere.position.x += movingVector[1]*0.1;*/
+        if(keys[90] && movingVector[0]<1) {movingVector[0] += 0.01}
+        if(keys[83] && movingVector[0]>-1) {movingVector[0] -= 0.01}
+        if(keys[68] && movingVector[1]<1) {movingVector[1] += 0.01}
+        if(keys[81] && movingVector[1]>-1) {movingVector[1] -= 0.01}
+
     };
+
+    // CURSOR MANAGEMENT
+    overlay.requestPointerLock = overlay.requestPointerLock || overlay.mozRequestPointerLock;
+    document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
+
+    overlay.onclick = function() {
+        overlay.requestPointerLock();
+        requestFullScreen(canvas);
+        fullScreen = true;
+    }
+
+    document.addEventListener("mousemove", updatePosition, false);
+    
+    
+
 });
+
+document.body.oncontextmenu = function() {
+    return false;
+}
+
+function updatePosition(e) {
+    if(fullScreen){
+        camera.rotation.x += e.movementY/1000
+        camera.rotation.y += e.movementX/10000
+
+        if(camera.rotation.x > 1.5){
+            camera.rotation.x = 1.5;
+        }
+
+        if(camera.rotation.x < -1.5){
+            camera.rotation.x = -1.5;
+        }
+    }
+
+    console.log(camera.rotation);
+  //console.log(e.movementX+" "+e.movementY);
+  
+}
+
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
